f95810e63102c087cfea2e87db2e5621776c84c6
Griffin <gmd298@gmail.com>
2022-07-23 18:18:44 -0400

    changed localhost to num.ber.s

--- src/config/index.js
+++ src/config/index.js
@@ -2,6 +2,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017';
+export const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017';
 export const port = process.env.PORT || 3000;
 export const logLevel = process.env.LOG_LEVEL || 'dev';
