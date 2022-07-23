b18eee33983bd222a4cc4f60f28018193c413ce0
Jacob Benton <jacob.a.benton@gmail.com>
2022-07-23 14:28:42 -0400

    Setup config with dotenv

--- src/server.js
+++ src/server.js
@@ -1,18 +1,19 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import { dbUrl, port } from './config';
 
 const app = express();
 
 export const connect = async () => {
   console.log('Connecting to mongo...');
-  await mongoose.connect('mongodb://localhost:27017');
+  await mongoose.connect(dbUrl);
   console.log('Successfully connected to mongo');
 };
 
 export const start = async () => {
   console.log('Starting server...');
-  await app.listen(3000);
-  console.log('Listening on port 3000...');
+  await app.listen(port);
+  console.log(`Listening on port ${port}...`);
 };
 
 export default app;
