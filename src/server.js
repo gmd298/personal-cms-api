0956fdcef5d093e2543301a8b1fa12737e0c4529
Griffin <gmd298@gmail.com>
2022-09-23 16:04:53 -0400

    trying to add CORS

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import morgan from 'morgan';
+import cors from 'cors';
 import logger from './config/logger';
 import v1Router from './v1';
 import { dbUrl, port, logLevel } from './config';
@@ -9,6 +10,9 @@ const app = express();
 app.use(morgan(logLevel));
 app.use(express.json());
 app.use('/v1', v1Router);
+app.use(cors({
+  origin: 'http://localhost:3001',
+}));
 
 export const connect = async () => {
   logger.info('Connecting to mongo...');
